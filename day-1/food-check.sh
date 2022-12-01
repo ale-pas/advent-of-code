5886fb52cfeaea475f6229b2b6b6445c68e06008
Alessandro Pascolini <alessandropasco@gmail.com>
2022-12-01 12:29:57 +0100

    first try

--- day-1/food-check.sh
+++ day-1/food-check.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo "24000 (carried by the fourth Elf)"
+file="elf-food.txt"
+
+array=( $(cat $file| sed -e 's/^$/-/') )
+count=0
+sum=0
+elf=0
+kal=0
+for i in "${array[@]}"
+do
+	if [[ "$i" == "-" ]]
+	then
+		if (( $kal < $sum ))
+		then
+			kal=$sum
+			elf=$count
+		fi
+		# echo "Elf $count calories: $sum"
+		let count+=1
+		sum=0
+	else
+		let sum+=$i
+	fi
+
+done
+echo "Elf $elf calories: $kal"
