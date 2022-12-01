c98451df53f238722bc58386d656bd06d598fedd
Alessandro Pascolini <alessandropasco@gmail.com>
2022-12-01 14:45:22 +0100

    second star

--- day-1/food-check.sh
+++ day-1/food-check.sh
@@ -5,7 +5,7 @@ file="elf-food.txt"
 array=( $(cat $file| sed -e 's/^$/-/') )
 count=0
 sum=0
-elf=0
+elfs=()
 kal=0
 for i in "${array[@]}"
 do
@@ -14,14 +14,17 @@ do
 		if (( $kal < $sum ))
 		then
 			kal=$sum
-			elf=$count
 		fi
 		# echo "Elf $count calories: $sum"
 		let count+=1
+		elfs+=("${sum}")
 		sum=0
 	else
 		let sum+=$i
 	fi
 
 done
+echo "${elfs[@]}"
+sorted=($(sort "${elfs[*]}"))
+echo "${sorted[@]}"
 echo "Elf $elf calories: $kal"
